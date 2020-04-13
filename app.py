189e712cf69b1b535f708717b1c44d200dbf7232
Julien PERON <julien.peron@protonmail.com>
2020-04-13 11:44:20 +0200

    Remove copy/past encrypt_bucket

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from src.api_gw_stack import ApiGatewayStack
 
 
 app = core.App()
-ApiGatewayStack(app, "ApiGatewayStack", env=core.Environment(region="eu-west-1"), encrypt_bucket=True)
+ApiGatewayStack(app, "ApiGatewayStack", env=core.Environment(region="eu-west-1"))
 
 app.synth()
