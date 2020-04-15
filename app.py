59781b00f6f91f15055cff343af8bce02424a35f
Julien PERON <julien.peron@protonmail.com>
2020-04-15 13:30:20 +0200

    Adding Cognito stack

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 
 from aws_cdk import core
 
+from src.cognito_stack import CognitoStack
 from src.api_gw_stack import ApiGatewayStack
 
 
 app = core.App()
+CognitoStack(app, "CognitoStack", env=core.Environment(region="eu-west-1"))
 ApiGatewayStack(app, "ApiGatewayStack", env=core.Environment(region="eu-west-1"))
 
 app.synth()
---
+++ src/cognito_stack.py
@@ -0,0 +1,19 @@
+from aws_cdk import core
+from aws_cdk import aws_cognito
+
+
+class CognitoStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        user_pool = aws_cognito.UserPool(self, "UserPool",
+                                         user_pool_name="Myawesomeapp-userpool",
+                                         self_sign_up_enabled=False)
+        # pool_id = user_pool.user_pool_id
+
+        api_client = user_pool.add_client("customer-app-client")
+        client_id = api_client.user_pool_client_id
+
+        cognito_idp = aws_cognito.CfnIdentityPool(self, "IdentityPool", identity_pool_name="Myawesomeapp-idp")
+        cognito_idp.CognitoIdentityProviderProperty(client_id=client_id)
